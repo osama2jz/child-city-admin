d31e75af1ace7e5e774ea8e4e3848fb4d3d5b0c1
Muhammad Usama <mosama4u@gmail.com>
2023-09-18 18:37:46 +0500

    link changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const backendUrl = "http://localhost:6969";
+export const backendUrl = "https://child-city-backend-a7000291e61e.herokuapp.com/";
 export const colors = [
   "Red",
   "Orange",
